dc4f44db94b7c82a3c74897c72c312081fbb678a
MarioAndra <marioandrawos75@gmail.com>
2025-03-23 21:02:40 +0300

    Rule Done

---
+++ app/Http/Controllers/Api/Dashboard/RuleController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Rule;
+use App\Traits\ResponseTrait;
+use App\Http\Requests\{
+    RuleRequest,
+    RuleUpdateRequest
+};
+use Whoops\Run;
+
+class RuleController extends Controller
+{
+    use ResponseTrait;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(RuleRequest $request)
+    {
+        $rule=Rule::create($request->all());
+        return $this->showResponse($rule,'create Done successfully....!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $rule=Rule::findOrFail($id);
+        return $this->showResponse($rule,'done');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(RuleUpdateRequest $request, string $id)
+    {
+        $rule=Rule::findOrFail($id);
+        $rule->update($request->all());
+        return $this->showResponse($rule,'update done successfully....!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $rule=Rule::findOrFail($id);
+        $rule->delete();
+        return $this->showMessage('delete done successfully....!');
+    }
+}
