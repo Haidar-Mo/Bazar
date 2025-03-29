026a62ea5a23d34e6473dbf80cc9b35fa9bc66eb
MarioAndra <marioandrawos75@gmail.com>
2025-03-29 04:23:16 +0300

    Chat

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -9,8 +9,10 @@ class ChatResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'sender_id' => $this->client->id,
             'sender_name' => $this->client->first_name,
             'sender_image' => $this->client->image,
+            'receiver_id' => $this->seller->id,
             'receiver_name' => $this->seller->first_name,
             'receiver_image' => $this->seller->image,
             'advertisement_details' => $this->advertisement_details,
