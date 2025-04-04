1851ff25aef03569087b68365d4d897b6f6a5432
MarioAndra <marioandrawos75@gmail.com>
2025-04-05 01:17:42 +0300

    plan mobile

--- routes/Mobile/V1/Plan.php
+++ routes/Mobile/V1/Plan.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Enums\TokenAbility;
-use App\Http\Controllers\Api\Mobile\SubscriptionController;
+use App\Http\Controllers\Api\Mobile\PlanController;
 use Illuminate\Support\Facades\Route;
 
 
-    Rout::middleware([
+    Route::middleware([
         'auth:sanctum',
         'ability:' . TokenAbility::ACCESS_API->value,
       //  'role:client'
     ])
     ->group(function () {
 
-           Route::apiResource('Subscriptions',SubscriptionController::class);
+           Route::apiResource('plans',PlanController::class);
 
     });
--- routes/api.php
+++ routes/api.php
@@ -36,5 +36,6 @@ Route::prefix('v1/')->group(function () {
     include __DIR__ . "/Mobile/V1/Meesage.php";
     include __DIR__ . "/Mobile/V1/City.php";
     include __DIR__ . "/Mobile/V1/Rule.php";
+    include __DIR__ . "/Mobile/V1/Plan.php";
 
 });
